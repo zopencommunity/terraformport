171af4abc10280a0c61c15ccd5890d1d4a00f694
Pradeepkumarreddy Chama <reddy.pradeep162@gmail.com>
2024-01-25 10:25:21 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-[![Automatic version updates](https://github.com/ZOSOpenTools/terraformport/actions/workflows/bump.yml/badge.svg)](https://github.com/ZOSOpenTools/terraformport/actions/workflows/bump.yml)
+terraform
 
-# terraform
+Terraform is an open-source infrastructure as code tool that enables you to safely and predictably provision and manage infrastructure in any cloud
---
+++ cicd-stable.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/terraformport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/terraformport.git'), string(name: 'PORT_DESCRIPTION', value: 'Terraform is an open-source infrastructure as code tool that enables you to safely and predictably provision and manage infrastructure in any cloud' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+  }
+}
