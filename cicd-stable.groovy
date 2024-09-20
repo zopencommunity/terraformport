c40af420cb5232fe571dde003c3ca49fd41e8022
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:36:29 -0400

    Change organization to zopencommunity

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/terraformport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/terraformport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/terraformport.git'), string(name: 'PORT_DESCRIPTION', value: 'Terraform is an open-source infrastructure as code tool that enables you to safely and predictably provision and manage infrastructure in any cloud' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/terraformport.git'), string(name: 'PORT_DESCRIPTION', value: 'Terraform is an open-source infrastructure as code tool that enables you to safely and predictably provision and manage infrastructure in any cloud' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
   }
 }
